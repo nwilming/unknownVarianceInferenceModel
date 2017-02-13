83cd0f58529523bf57ca2296be382e5042cbca92
Niklas Wilming <niklaswilming@fastmail.fm>
2017-02-13 09:43:39 +0100

    Typos

--- src/fits_module.py
+++ src/fits_module.py
@@ -17,6 +17,7 @@ import enum, os, sys, math, scipy, pickle, warnings, json, logging, logging.conf
 import scipy.signal
 import numpy as np
 from utils import normpdf,average_downsample,parse_details_file
+import utils
 
 try:
 	with warnings.catch_warnings():
@@ -427,7 +428,7 @@ _fit_output = {_fit_output}
 				self.external_var = dat['variance']/self._ISI
 			except:
 				raise RuntimeError('Cannot perform fits for unknown variance DecisionModel if the data does not have a "variance" field')
-			self.unique_stim,self.stim_indeces,counts = utils.unique_rows(np.array([self.contrast,self.external_var]).T,return_inverse=True,return_counts=True)
+			self.unique_stim,self.stim_indeces,count = utils.unique_rows(np.array([self.contrast,self.external_var]).T,return_inverse=True,return_counts=True)
 			self.stim_probs = count.astype(np.float64)/np.sum(count.astype(np.float64))
 		self.logger.debug('Trials loaded = %d',len(self.performance))
 		self.mu,self.mu_indeces,count = np.unique(self.contrast,return_inverse=True,return_counts=True)
